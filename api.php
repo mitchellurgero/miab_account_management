bb17a1a1d7efe9061ba5977dfdd0b6a1262a65f2
mitchellurgero <info@urgero.org>
2019-04-08 19:48:35 +0000

    Fix big boo boo in regex.

--- api.php
+++ api.php
@@ -113,7 +113,7 @@ if(isset($_POST['t'])){
 function clean($string) {
    $string = str_replace(' ', '-', $string); // Replaces all spaces with hyphens.
    //$string = str_replace(".","",$string);
-   $string = preg_replace('/[^A-Za-z0-9\-]/', '', $string); // Removes special chars.
+   $string = preg_replace('/[^A-Za-z0-9\-\.]/', '', $string); // Removes special chars.
    return preg_replace('/-+/', '-', $string); // Replaces multiple hyphens with single one.
 }
 ?>
--- index.php
+++ index.php
@@ -196,7 +196,7 @@ if(isset($_POST['username']) && isset($_POST['t'])){
 <?php
 function clean($string) {
    $string = str_replace(' ', '-', $string); // Replaces all spaces with hyphens.
-   $string = preg_replace('/[^A-Za-z0-9\-]/', '', $string); // Removes special chars.
+   $string = preg_replace('/[^A-Za-z0-9\-\.]/', '', $string); // Removes special chars.
    return preg_replace('/-+/', '-', $string); // Replaces multiple hyphens with single one.
 }
 function generateRandomString($length = 10) {
